c360e0e2488c8da7c67419c1bff471df6fe4e6d7
Gigi M <mmgamao1@gmail.com>
2019-04-23 15:34:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your code in this file!
+
+
+let currentUser = 'Gigi';
+
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase + currentUser.toUpperCase + "!";
+
+//let shortGreeting = "Welcome, " + 
+//     it("contains the first initial of the name stored in the 'currentUser' variable", () => {
+//       const firstInitial = currentUser[0];
+//       const restOfName = currentUser.slice(1);
+
+//       expect(shortGreeting).to.have.string(firstInitial);
+//       expect(shortGreeting).to.not.have.string(restOfName);
+//     });
+
+//     it('ends with an exclamation point', () => {
+//       expect(shortGreeting.substr(-1)).to.eq('!');
+//     });
+//   });
+// });
